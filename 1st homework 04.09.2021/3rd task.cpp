730d5c493c5f8dc3e95c341398644bb623a7e1b1
Greatsol <ilia2309ilia@gmail.com>
2021-09-08 21:36:52 +0300

    [fix]: 3rd task

--- 1st homework 04.09.2021/3rd task.cpp
+++ 1st homework 04.09.2021/3rd task.cpp
@@ -4,11 +4,8 @@
 #include <limits>
 
 
-double check_for_null(double coef) {
-    if (abs(coef) < std::numeric_limits<double>::epsilon()) {
-        coef = 0;
-    }
-    return coef;
+bool is_equal(double x, double y) { 
+    return std::abs(x - y) < std::numeric_limits<double>::epsilon();
 }
 
 
@@ -24,26 +21,23 @@ int main() {
     std::cout << "Coefficient c: ";
     std::cin >> coef_c;
 
-    coef_a = check_for_null(coef_a);
-    coef_b = check_for_null(coef_b);
-    coef_c = check_for_null(coef_c);
 
-    if (coef_a == 0) {
-        if (coef_b == 0 || coef_c == 0) {
+    if (is_equal(coef_a, 0.0)) {
+        if (is_equal(coef_b, 0.0) || is_equal(coef_c, 0.0)) {
             std::cout << "Unlimited number of solutions.";
         }
-        else if (coef_b == 0) {
+        else if (is_equal(coef_b, 0.0)) {
             std::cout << "No solution.";
         }
-        else if (coef_c == 0) {
+        else if (is_equal(coef_c, 0.0)) {
             std::cout << "x = 0";
         }
         else {
             std::cout << "x = " << -coef_c / coef_b;
         }
     }
-    else if (coef_b == 0) {
-        if (coef_c == 0) {
+    else if (is_equal(coef_b, 0.0)) {
+        if (is_equal(coef_c, 0.0)) {
             std::cout << "x = 0";
         }
         else if (coef_c < 0) {
@@ -53,7 +47,7 @@ int main() {
             std::cout << "No solution.";
         }
     }
-    else if (coef_c == 0) {
+    else if (is_equal(coef_c, 0.0)) {
         x1 = 0;
         x2 = -coef_b / coef_a;
         std::cout << "x1 = " << x1 << ", x2 = " << x2;
@@ -61,15 +55,13 @@ int main() {
     else {
         double D;
         D = coef_b * coef_b - 4 * coef_a * coef_c;
-        if (D >= 0) {
+        if (is_equal(D, 0.0)) {
+            std::cout << "x1 = x2 = " << -coef_b / (2 * coef_c);
+        }
+        else if (D > 0) {
             x1 = (-coef_b + sqrt(D)) / (2 * coef_a);
             x2 = (-coef_b - sqrt(D)) / (2 * coef_a);
-            if (x1 == x2) {
-                std::cout << "x1 = x2 = " << x1;
-            }
-            else {
-                std::cout << "x1 = " << x1 << ", x2 = " << x2;
-            }
+            std::cout << "x1 = " << x1 << ", x2 = " << x2;
         }
         else {
             std::cout << "No solution.";
