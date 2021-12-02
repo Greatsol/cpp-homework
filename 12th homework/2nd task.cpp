191b56514531720ef81f39c0fdeb2890018a118c
Greatsol <ilia2309ilia@gmail.com>
2021-12-02 23:01:47 +0300

    [fix]: 2nd task hw12

--- 12th homework/2nd task.cpp
+++ 12th homework/2nd task.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 
 
-template<unsigned n>
+template<int n>
 struct number_fbi {
-	const int number = number_fbi<n - 1>::number + F<n - 2>::number;
+	static inline const int number = number_fbi<n - 1>::number + number_fbi<n - 2>::number;
 };
 
 
 template<>
 struct number_fbi<0> {
-	const int number = 0;
+	static inline const int number = 0;
 };
 template<>
 struct number_fbi<1> {
-	const int number = 1;
+	static inline const int number = 1;
 };
-template<>
-struct number_fbi<2> {
-	const int number = 1;
-};
+
 
 
 int main() {
